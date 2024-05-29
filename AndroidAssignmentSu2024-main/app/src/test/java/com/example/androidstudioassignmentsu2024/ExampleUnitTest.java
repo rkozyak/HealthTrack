8ee54f595f206ebcb03ae13d608708f553d00439
Alexander Wang <awang652@gatech.edu>
2024-05-29 11:13:07 -0400

    Created unit test (TODO7)

--- AndroidAssignmentSu2024-main/app/src/test/java/com/example/androidstudioassignmentsu2024/ExampleUnitTest.java
+++ AndroidAssignmentSu2024-main/app/src/test/java/com/example/androidstudioassignmentsu2024/ExampleUnitTest.java
@@ -14,5 +14,15 @@ public class ExampleUnitTest {
     // TODO 7: Write a unit test for ensuring correct behavior of the addFlashcard function you implemented in TODO 2 in FlashcardManagerSingletons
     @Test
     public void add_flashcard_test() {
+	FlashcardManagerSingleton flashcardTest = new FlashcardManagerSingleton();
+	Flashcard flashcard = new Flashcard("1+1","2");
+	Flashcard flashcard2 = new Flashcard("2+2","4");
+	flashcardTest.addFlashcard(flashcard);
+	flashcardTest.addFlashcard(flashcard2);
+	for(int i = 0; i < flashcardTest.getFlashcards(); i++) {
+	    System.out.println("Flashcard #" + (i + 1) + ":");
+	    System.out.println("Question: " + flashcardTest.getFlashcards().get(i).getQuestion());
+	    System.out.println("Answer: " + flashcardTest.getFlashcards().get(i).getAnswer());
+	}
     }
 }
