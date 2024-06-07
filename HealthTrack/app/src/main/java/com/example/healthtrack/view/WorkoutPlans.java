d5f68cf914e9b70e5873c851a6f2356a1aaf3d14
jvchblade <brian078910@gmail.com>
2024-06-07 14:32:14 -0400

    Made the main feature activities instead of regular classes

--- HealthTrack/app/src/main/java/com/example/healthtrack/view/WorkoutPlans.java
+++ HealthTrack/app/src/main/java/com/example/healthtrack/view/WorkoutPlans.java
@@ -1,4 +1,26 @@
 package com.example.healthtrack.view;
 
-public class WorkoutPlans {
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.healthtrack.R;
+
+public class WorkoutPlans extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_workout_plans);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 }
