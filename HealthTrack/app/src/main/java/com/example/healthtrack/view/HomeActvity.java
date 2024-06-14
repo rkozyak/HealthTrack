01a2e3593c83005b10d5cdaa808899283205a599
rkozyak <rkozyak@gmail.com>
2024-06-14 13:01:26 -0400

    Made the home buttons do stuff

--- HealthTrack/app/src/main/java/com/example/healthtrack/view/HomeActvity.java
+++ HealthTrack/app/src/main/java/com/example/healthtrack/view/HomeActvity.java
@@ -1,6 +1,9 @@
 package com.example.healthtrack.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,5 +20,41 @@ public class HomeActvity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_actvity);
+
+        // Calorie Button
+        Button openCalorieButton = findViewById(R.id.caloriesButton);
+        openCalorieButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent  = new Intent(HomeActvity.this, CalorieTracking.class);
+                startActivity(intent);
+            }
+        });
+
+        // Tracker Button
+        Button openTrackerButton = findViewById(R.id.trackerButton);
+        openTrackerButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent  = new Intent(HomeActvity.this, WorkoutTracker.class);
+                startActivity(intent);
+            }
+            });
+
+        // Workouts Button
+        Button openWorkoutsButton = findViewById(R.id.workoutsButton);
+        openWorkoutsButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent  = new Intent(HomeActvity.this, WorkoutPlans.class);
+                startActivity(intent);
+            }
+            });
+
+        // Community Button
+        Button openCommunityButton = findViewById(R.id.communityButton);
+        openCommunityButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent  = new Intent(HomeActvity.this, CommunityScreen.class);
+                startActivity(intent);
+            }
+            });
     }
 }
