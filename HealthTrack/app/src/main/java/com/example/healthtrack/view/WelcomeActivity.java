bab2c573ba43d3790f614e09ea146fc974f90117
rkozyak <rkozyak@gmail.com>
2024-06-14 13:52:40 -0400

    Smashed bug and updated XML

--- HealthTrack/app/src/main/java/com/example/healthtrack/view/WelcomeActivity.java
+++ HealthTrack/app/src/main/java/com/example/healthtrack/view/WelcomeActivity.java
@@ -49,7 +49,6 @@ public class WelcomeActivity extends AppCompatActivity {
         });
 
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_welcome);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
