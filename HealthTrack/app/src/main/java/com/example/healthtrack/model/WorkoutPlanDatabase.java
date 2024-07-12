65552e937f4cfb2f1ee0f0a5ae09f38b0288639c
Alex Wang <awang652@gatech.edu>
2024-07-12 18:08:52 -0400

    Separate workoutplandatabase created

---
+++ HealthTrack/app/src/main/java/com/example/healthtrack/model/WorkoutPlanDatabase.java
@@ -0,0 +1,29 @@
+package com.example.healthtrack.model;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class WorkoutPlanDatabase {
+    private static WorkoutPlanDatabase instance;
+    private DatabaseReference db;
+
+    private WorkoutPlanDatabase() {
+        db = FirebaseDatabase.getInstance().getReference("workouts");
+    }
+
+    public static WorkoutPlanDatabase getInstance() {
+        if (instance == null) {
+            synchronized (WorkoutPlanDatabase.class) {
+                if (instance == null) {
+                    instance = new WorkoutPlanDatabase();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    public DatabaseReference getDatabaseReference() {
+        return db;
+    }
+}
---
+++ HealthTrack/app/src/main/java/com/example/healthtrack/model/WorkoutPlanDatabaseRepository.java
@@ -0,0 +1,28 @@
+package com.example.healthtrack.model;
+
+import com.google.firebase.database.DatabaseReference;
+
+public class WorkoutPlanDatabaseRepository {
+    private WorkoutPlanDatabase workoutPlanDatabase;
+    private DatabaseReference db;
+
+    public WorkoutPlanDatabaseRepository() {
+        workoutPlanDatabase = WorkoutPlanDatabase.getInstance();
+        db = workoutPlanDatabase.getDatabaseReference();
+    }
+
+    public void addWorkout(String userId, Workout workout,
+                           DatabaseReference.CompletionListener completionListener) {
+        DatabaseReference userRef = db.child(userId);
+        String workoutId = userRef.push().getKey();
+
+        if (workoutId != null) {
+            userRef.child(workoutId).setValue(workout, completionListener);
+        }
+    }
+
+    //Note: Gets USER SPECIFIC workouts
+    public DatabaseReference getWorkoutsReference(String userId) {
+        return db.child(userId);
+    }
+}
