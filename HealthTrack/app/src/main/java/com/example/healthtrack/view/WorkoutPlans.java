37cb6b260d7a2720356e270341296a74b9a506da
Christian <cchin45@gatech.edu>
2024-07-13 18:55:09 -0400

    Added popout workout plan screen

--- HealthTrack/app/src/main/java/com/example/healthtrack/view/WorkoutPlans.java
+++ HealthTrack/app/src/main/java/com/example/healthtrack/view/WorkoutPlans.java
@@ -1,8 +1,10 @@
 package com.example.healthtrack.view;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -11,10 +13,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.healthtrack.R;
 
 public class WorkoutPlans extends AppCompatActivity {
+    Dialog dialog;
+    Button btnDialogAdd;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_workout_plans);
+
+        dialog = new Dialog(WorkoutPlans.this);
+        dialog.setContentView(R.layout.workout_plan_popout);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.custom_dialog_bg));
+        dialog.setCancelable(false);
+
+        btnDialogAdd = dialog.findViewById(R.id.publishPlan);
+
+        btnDialogAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
 
         Button backButton = findViewById(R.id.btn_plans_back);
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -58,6 +77,14 @@ public class WorkoutPlans extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent  = new Intent(WorkoutPlans.this, CommunityScreen.class);
                 startActivity(intent);
+            }
+        });
+
+        Button createPlan = findViewById(R.id.plan_btn);
+        createPlan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.show();
             }
         });
     }
