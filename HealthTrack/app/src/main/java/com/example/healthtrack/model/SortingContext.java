b8037af37cb4c42212797536beb8ad91ee3f141a
Alex Wang <awang652@gatech.edu>
2024-07-14 21:28:02 -0400

    Strategy design pattern

---
+++ HealthTrack/app/src/main/java/com/example/healthtrack/model/SortingContext.java
@@ -0,0 +1,11 @@
+package com.example.healthtrack.model;
+
+import com.example.healthtrack.model.SortingStrategy;
+
+import java.util.ArrayList;
+
+public class SortingContext {
+    public ArrayList<WorkoutPlan> filter(SortingStrategy strategy, ArrayList<WorkoutPlan> list) {
+        return strategy.sort(list);
+    }
+}
