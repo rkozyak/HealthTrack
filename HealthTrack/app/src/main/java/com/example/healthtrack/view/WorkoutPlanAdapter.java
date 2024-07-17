38903cda5d5b8a1cbd43febb41365eebbbc977f0
jvchblade <brian078910@gmail.com>
2024-07-16 23:56:05 -0400

    Added the Add Workout functionality

--- HealthTrack/app/src/main/java/com/example/healthtrack/view/WorkoutPlanAdapter.java
+++ HealthTrack/app/src/main/java/com/example/healthtrack/view/WorkoutPlanAdapter.java
@@ -3,13 +3,20 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.healthtrack.R;
+import com.example.healthtrack.model.Workout;
 import com.example.healthtrack.model.WorkoutPlan;
+import com.example.healthtrack.viewModel.WorkoutViewModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 
@@ -68,6 +75,8 @@ public class WorkoutPlanAdapter extends RecyclerView.Adapter<WorkoutPlanAdapter.
         private TextView calories;
         private TextView notes;
         private TextView logged;
+        private Button addButton;
+        private FirebaseAuth mAuth;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -80,6 +89,32 @@ public class WorkoutPlanAdapter extends RecyclerView.Adapter<WorkoutPlanAdapter.
             calories = itemView.findViewById(R.id.caloriesItem);
             notes = itemView.findViewById(R.id.notesItem);
             logged = itemView.findViewById(R.id.logged);
+
+            addButton = itemView.findViewById(R.id.addPlanItem);
+            addButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (logged.getText().toString().equals("Logged")) {
+                        Toast.makeText(v.getContext(), "Already logged", Toast.LENGTH_SHORT).show();
+                    } else {
+                        mAuth = FirebaseAuth.getInstance();
+                        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+                        String userId = currentUser.getUid();
+
+                        String workoutNameItem = workoutName.getText().toString();
+                        int caloriesItem = Integer.parseInt(calories.getText().toString());
+                        int setsItem = Integer.parseInt(sets.getText().toString());
+                        int repsItem = Integer.parseInt(reps.getText().toString());
+                        String notesItem = notes.getText().toString();
+                        Workout workout = new Workout(userId, workoutNameItem, caloriesItem,
+                                setsItem, repsItem, notesItem);
+                        WorkoutViewModel workoutViewModel = new WorkoutViewModel();
+                        workoutViewModel.addWorkout(userId, workout);
+                        logged.setText("Logged");
+                    }
+                }
+            });
         }
     }
 }
