c73a084d63709452d380a9a21a377401aed148aa
“Eric-Shao” <yshao91@gatech.edu>
2024-07-24 15:16:16 -0400

    Implemented addChallenge

--- HealthTrack/app/src/main/java/com/example/healthtrack/model/ChallengeDatabaseRepository.java
+++ HealthTrack/app/src/main/java/com/example/healthtrack/model/ChallengeDatabaseRepository.java
@@ -10,4 +10,14 @@ public class ChallengeDatabaseRepository {
         challengeDatabase = ChallengeDatabase.getInstance();
         db = challengeDatabase.getDatabaseReference();
     }
+
+    public void addChallenge(String userId, CommunityChallenge communityChallenge,
+                           DatabaseReference.CompletionListener completionListener) {
+        DatabaseReference userRef = db.child(userId);
+        String workoutId = userRef.push().getKey();
+
+        if (workoutId != null) {
+            userRef.child(workoutId).setValue(communityChallenge, completionListener);
+        }
+    }
 }
