2c5b78ad4d2aa3291a42d8df0856135143983440
Richard K. <rkozyak@gmail.com>
2024-07-25 17:48:40 -0400

    Fixed checkstyle

--- HealthTrack/app/src/main/java/com/example/healthtrack/model/SortingContext.java
+++ HealthTrack/app/src/main/java/com/example/healthtrack/model/SortingContext.java
@@ -1,7 +1,5 @@
 package com.example.healthtrack.model;
 
-import com.example.healthtrack.model.SortingStrategy;
-
 import java.util.ArrayList;
 
 public class SortingContext {
--- HealthTrack/app/src/main/java/com/example/healthtrack/view/WorkoutPlanAdapter.java
+++ HealthTrack/app/src/main/java/com/example/healthtrack/view/WorkoutPlanAdapter.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.healthtrack.R;
@@ -20,8 +19,6 @@ import com.example.healthtrack.model.WorkoutPlan;
 import com.example.healthtrack.viewModel.WorkoutViewModel;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
